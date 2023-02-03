0d5149108f60161ef000a4a144b8a4945de8c0f8
muhammad ilham <ilham.mancity13@gmail.com>
2023-02-03 12:39:08 +0700

    commit

---
+++ databasebackup/paw_flask.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 19, 2019 at 03:15 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `paw_flask`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bukutelepon`
+--
+
+CREATE TABLE `bukutelepon` (
+  `no` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `no_telp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bukutelepon`
+--
+
+INSERT INTO `bukutelepon` (`no`, `nama`, `no_telp`) VALUES
+(2, 'eqw', 'eqw'),
+(4, '23w', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftarmhs`
+--
+
+CREATE TABLE `daftarmhs` (
+  `no` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `nim` varchar(20) NOT NULL,
+  `prodi` varchar(20) NOT NULL,
+  `fakultas` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `daftarmhs`
+--
+
+INSERT INTO `daftarmhs` (`no`, `nama`, `nim`, `prodi`, `fakultas`) VALUES
+(3, ' 12 12', '123', ' 12 123', ' 12 321'),
+(5, 'wqe', 'weq', 'qwe', 'weq');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bukutelepon`
+--
+ALTER TABLE `bukutelepon`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indexes for table `daftarmhs`
+--
+ALTER TABLE `daftarmhs`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bukutelepon`
+--
+ALTER TABLE `bukutelepon`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `daftarmhs`
+--
+ALTER TABLE `daftarmhs`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
